683ce103c0400c93b462ca3d84810babf79d2acf
xanzadu <noahjlehman@gmail.com>
2021-11-18 17:25:37 -0800

    fixed front end to match backend

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const { outfitData } = require('./clientOutfit.js');
 const app = express();
 const PORT = 3000 || process.env.PORT;
 const URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo';
-const questionsURL = 'http://localhost:8080'
+const productURL = 'http://localhost:8080';
 const HEADERS = { headers: { Authorization: GH_TOKEN } };
 
 app.use(express.static('./client'));
@@ -18,7 +18,7 @@ const customerOutfit = outfitData;
 
 app.get('/products', (req, res) => {
   const { page, count } = req.query;
-  const url = `${URL}/products/?page=${page}&count=${count}`;
+  const url = `${productURL}/products/?page=${page}&count=${count}`;
 
   axios.get(url, HEADERS)
     .then((response) => {
@@ -31,7 +31,7 @@ app.get('/products', (req, res) => {
 
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
-  const url = `${URL}/products/${id}`;
+  const url = `${productURL}/products/${id}`;
   console.log(url)
 
   axios.get(url, HEADERS)
@@ -45,7 +45,7 @@ app.get('/products/:id', (req, res) => {
 
 app.get('/products/:id/styles', (req, res) => {
   const { id } = req.params;
-  const url = `${URL}/products/${id}/styles`;
+  const url = `${productURL}/products/${id}/styles`;
 
   axios.get(url, HEADERS)
     .then((response) => {
@@ -58,8 +58,8 @@ app.get('/products/:id/styles', (req, res) => {
 
 app.get('/products/:id/details', (req, res) => {
   const { id } = req.params;
-  const url1 = `${URL}/products/${id}/`;
-  const url2 = `${URL}/products/${id}/styles`;
+  const url1 = `${productURL}/products/${id}/`;
+  const url2 = `${productURL}/products/${id}/styles`;
 
   axios.get(url1, HEADERS)
     .then((productInfoResponse) => {
@@ -76,7 +76,7 @@ app.get('/products/:id/details', (req, res) => {
 
 app.get('/products/:id/related', (req, res) => {
   const { id } = req.params;
-  const url = `${URL}/products/${id}/related`;
+  const url = `${productURL}/products/${id}/related`;
 
   axios.get(url, HEADERS)
     .then((response) => {
@@ -183,7 +183,7 @@ app.post('/reviews/', (req, res) => {
 app.get('/qa/questions/:id', (req, res) => {
   const { id } = req.params;
   const { count } = req.query;
-  const url = `${questionsURL}/qa/questions/?product_id=${id}`;
+  const url = `${URL}/qa/questions/?product_id=${id}`;
   const headers = { params: { count }, ...HEADERS };
 
   axios.get(url, headers)
@@ -210,7 +210,7 @@ app.get('/qa/:question_id/answers', (req, res) => {
 
 app.post('/qa/questions', (req, res) => {
   const { body } = req;
-  const url = `${questionsURL}/qa/questions`;
+  const url = `${URL}/qa/questions`;
 
   axios.post(url, body, HEADERS)
     .then((response) => {
@@ -224,7 +224,7 @@ app.post('/qa/questions', (req, res) => {
 app.post('/qa/:question_id/answers', (req, res) => {
   const { body } = req;
   // eslint-disable-next-line camelcase
-  const url = `${questionsURL}/qa/questions/${req.params.question_id}/answers`;
+  const url = `${URL}/qa/questions/${req.params.question_id}/answers`;
   axios.post(url, body, HEADERS)
     .then((response) => {
       res.status(response.status).send(response.data);
@@ -249,7 +249,7 @@ app.post('/cart', (req, res) => {
 });
 
 app.put('/qa/questions/:question_id/helpful', (req, res) => {
-  const url = `${questionsURL}/qa/questions/${req.params.question_id}/helpful`;
+  const url = `${URL}/qa/questions/${req.params.question_id}/helpful`;
   axios.put(url, null, HEADERS)
     .then((response) => {
       res.status(response.status).send(response.data);
@@ -260,7 +260,7 @@ app.put('/qa/questions/:question_id/helpful', (req, res) => {
 });
 
 app.put('/qa/answers/:answer_id/helpful', (req, res) => {
-  const url = `${questionsURL}/qa/answers/${req.params.answer_id}/helpful`;
+  const url = `${URL}/qa/answers/${req.params.answer_id}/helpful`;
 
   axios.put(url, {}, HEADERS)
     .then(({ status, body }) => {
@@ -272,7 +272,7 @@ app.put('/qa/answers/:answer_id/helpful', (req, res) => {
 });
 
 app.put('/qa/questions/:question_id/report', (req, res) => {
-  const url = `${questionsURL}/qa/questions/${req.params.question_id}/report`;
+  const url = `${URL}/qa/questions/${req.params.question_id}/report`;
 
   axios.put(url, null, HEADERS)
     .then((response) => {
@@ -284,7 +284,7 @@ app.put('/qa/questions/:question_id/report', (req, res) => {
 });
 
 app.put('/qa/answers/:answer_id/report', (req, res) => {
-  const url = `${questionsURL}/qa/answers/${req.params.answer_id}/report`;
+  const url = `${URL}/qa/answers/${req.params.answer_id}/report`;
 
   axios.put(url, null, HEADERS)
     .then((response) => {
